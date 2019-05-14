6dde7fb0cbb9ab95caae325caa6659e1602d287a
antoniopedrodantas <pedro.dantascv@gmail.com>
2019-05-14 15:16:37 +0100

    Testing user pt2

--- src/args.c
+++ src/args.c
@@ -7,54 +7,47 @@
 
 
 
-int get_arguments(int argc, const* argv[],instruction *data){
+int get_arguments(int argc, char* argv[],instruction *data){
 
     //process id
-    if(!isdigit(atoi(argv[1]))){
-        printf("not an integer\n");
-        return -1;
-    }
-    set_id(data,argv[1]);
+    
+    // falta testar se é valido
+    set_id(data, atoi(argv[1]));
 
 
 
-  //process password
+    //process password
     int length = strlen(argv[2]);
-
-    if( (argv[2])[0] != '"' || (argv[2])[length-1] != '"'){
-        printf("not valid format\n");
-        return -1;
-    }
 
     if(length-2 < MIN_PASSWORD_LEN || length-2 > MAX_PASSWORD_LEN){
         printf("size not allowed\n");
         return -1;
     }
-    set_password(data,argv[2]);
+    set_password(data, argv[2]);
 
 
     //process latency
-    if(argv[3]> MAX_OP_DELAY_MS){
+    if(atoi(argv[3]) > MAX_OP_DELAY_MS){
         printf("latency not allowed\n");
         return -1;
     }
-    set_latency(data,argv[3]);
+    set_latency(data, atoi(argv[3]));
 
 
     //process instruction
-    if(argv[4] < 0 || argv[4] > 3){
+    if(atoi(argv[4]) < 0 || atoi(argv[4]) > 3){
         printf("operation not allowed\n");
         return -1;
     }
 
-    set_operation(data,argv[4]);
+    set_operation(data, atoi(argv[4]));
 
     //process flag
-    if(argv[4]== 1 || argv[4] == 3){
+    if(atoi(argv[4]) == 1 || atoi(argv[4]) == 3){
         set_flag(data,false);
     }
 
-    if(argv[4]== 0 || argv[4] == 2){
+    if(atoi(argv[4]) == 0 || atoi(argv[4]) == 2){
         set_flag(data,true);
     }
 
