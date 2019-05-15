c4723d5e1ca0474a59ddfb3ba2fc4fae1dfd20e1
Bernardo Santos <bernardosantos670@gmail.com>
2019-05-15 01:13:46 +0100

    Updated server

--- src/server/server.c
+++ src/server/server.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 #include <sys/types.h>
@@ -19,6 +20,7 @@ int main(int argc, char* argv[]) {
         printf("Invalid arguments! \n");
         return ARGS_ERROR;
     }
+    
 
     int num_offices = atoi(argv[1]);
     if (num_offices <= 0 || num_offices > MAX_BANK_OFFICES) {
@@ -28,17 +30,13 @@ int main(int argc, char* argv[]) {
 
 
     /* Check if the admin's password respects the desired conditions */
-    // TODO: check if this is the correct way of dealing with the password, account for the quotes or not?
-    char admin_password[MAX_PASSWORD_LEN + 3];
-    if (argv[2][0] != '"' || argv[2][strlen(argv[2])] != '"') {
-        printf("Password must be written inside quotes (\"example\") \n");
-        return ARGS_ERROR;
-    }
-    if (strlen(argv[2]) < MIN_PASSWORD_LEN - 2 || strlen(argv[2]) > MAX_PASSWORD_LEN - 2) {
+    // TODO: check if this is the correct way of dealing with the password, account for the quotes or not?    
+    if (strlen(argv[2]) < MIN_PASSWORD_LEN || strlen(argv[2]) > MAX_PASSWORD_LEN) {
         printf("Invalid password! Must have at least %d characters and a maximum of %d. \n", MIN_PASSWORD_LEN, MAX_PASSWORD_LEN);
         return ARGS_ERROR;
     }
-    admin_password[strlen(admin_password)] = '\0';
+    char admin_password[MAX_PASSWORD_LEN + 1];
+    admin_password[MAX_PASSWORD_LEN] = '\0';
     /* Admin's password is valid */
 
     
@@ -74,15 +72,29 @@ int main(int argc, char* argv[]) {
     /* Logfile is created and open */
 
 
+    /* Create a request queue, for the server to store the requests */
+    request_queue_t *queue = create_request_queue(num_offices);
+    if (queue == NULL) {
+        printf("Could not create a queue for the requests! \n");
+        return 1;
+    }
+    pthread_mutex_t queue_mutex;
+    pthread_mutex_init(&queue_mutex, NULL);                         // TODO: deal with errors
+    /* Queue is created and ready for use */
+
+
     /* Create counter threads */
-    pthread_t threads[num_offices];
-    for (int i = 1; i < num_offices; i++) {
+    pthread_t threads[num_offices + 1];
+    threads[MAIN_THREAD_ID] = MAIN_THREAD_ID;
+    for (int i = 1; i <= num_offices; i++) {
         threads[i] = i;
 
         office_args_t *args = malloc(sizeof(office_args_t));
         args->log_fd = logfile_fd;
         args->id = i;
         args->shutdown = &shutdown;
+        args->queue = queue;
+        args->queue_mutex = &queue_mutex;
 
         pthread_create(&threads[i], NULL, office_main, args);       // TODO: deal with errors
     }
@@ -97,13 +109,38 @@ int main(int argc, char* argv[]) {
 
     /* Loop for the server to receive messages
        If the server is ordered to shutdown this loop will end */
+    int request_size = sizeof(tlv_request_t);
+    char request_buf[request_size];
+    
     while (!shutdown) {
+        if (is_full(queue)) {
+            continue;
+        }
 
+        if (read(request_fd, request_buf, request_size) > 0) {     // TODO: deal with errors
+            tlv_request_t *request = (tlv_request_t *) (uintptr_t) atoi(request_buf);       // TODO: ask teacher
+            
+            pthread_mutex_lock(&queue_mutex);
+            if (logSyncMech(logfile_fd, MAIN_THREAD_ID, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_PRODUCER, request->value.header.pid) < 0) {
+                printf("Error writing to logfile! \n");
+            }
+            
+            if (push(queue, request)) {
+                printf("Could not add request to queue, queue is full! \n");
+            }
+
+            pthread_mutex_unlock(&queue_mutex);
+            if (logSyncMech(logfile_fd, MAIN_THREAD_ID, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_PRODUCER, request->value.header.pid) < 0) {
+                printf("Error writing to logfile! \n");
+            }
+        }
     }
 
 
-    /* Close request FIFO because the shutdown command was received */
+    /* Close and request FIFO from systme because the shutdown command was received */
     close(request_fd);
+    remove(SERVER_FIFO_PATH);
+
 
     /* Wait for all requests in the queue to be processed */
     for (int i = 0; i < num_offices; i++) {
